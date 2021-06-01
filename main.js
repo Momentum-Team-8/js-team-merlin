63fe3fe31665ea893bc8905deae762955b5f20cc
Emily Foley <efoley326@gmail.com>
2021-06-01 15:41:35 -0400

    almost generating movie titles

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
-const url = "http://localhost:3000"
+const url = "http://localhost:3000/movies"
 
 const form = document.querySelector(".form")
 
 const movieText = document.querySelector("#New-Movie")
 
-function createMovie (text) {
-    fetch (url {
-        method: "POST",
-        headers: {"Content-Type":application/JSON}
-        body: JSON.stringify({
-            title: text, 
-            body: text,
-            create_at: moment().format()
+const movieTitle = document.querySelector(".movies")
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    createMovie(movieText.value)
+})
+
+function createMovie (text) {
+    console.log(text)
+    fetch (url, {
+        method: "POST",
+        headers: {"Content-Type" : "application/JSON"} ,
+        body: JSON.stringify({
+            title: text,
+            create_at: moment().format(),
+            watched: false
         })
-    })
-.then (response => response.json())
-.then (data => renderMovies(data))
+        })
+        .then (response => response.json())
+        .then (data => renderMovie(data))
+}
+
+function renderMovie (data) {
+    const movieList = document.createElement("li")
+    movieList.id = data.id
+    renderMovieText(movieList, data) 
+    movieTitle.appendChild(movieList)
+}
+
+function renderMovieText(movieList, data) 
+{ 
+    const Title = document.createElement ("p")
+    movieList.innerHTML = data.title
+    const watchedButton = document.createElement ("button")
+    watchedButton.classList.add ("watched")
+    movieList.appendChild(Title)
+}
+
+function createListItem() {
+    console.log('create movie called')
+    let movie = document.querySelector('input').value
+    renderMovie(movie)
+    console.log(movie)
+}
+
+function getlistofMovies ()
+{
+    console.log('confirm list')
+    fetch (url)
+    .then (resp =>
+        resp.json()    )
+        .then(data =>  { 
+            for (let x of data) {
+                renderMovie(x)
+            }
+        } )
+        ;
+}
+
+getlistofMovies()
+createListItem()
 
