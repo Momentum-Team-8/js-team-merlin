24bb7a8d9c3e9d31edd16ecd8c7b25292fe822f0
Emily Foley <efoley326@gmail.com>
2021-06-01 11:25:22 -0400

    function addition

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const form = document.querySelector(".form")
 const movieText = document.querySelector("#New-Movie")
 
 function createMovie (text) {
-    fetch (url) {
+    fetch (url {
         method: "POST",
         headers: {"Content-Type":application/JSON}
-    }
-}
+        body: JSON.stringify({
+            title: text, 
+            body: text,
+            create_at: moment().format()
+
+        })
+    })
+.then (response => response.json())
+.then (data => renderMovies(data))
+
