c858c11131d4f12a759d0477e90430e670f802f3
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-01 11:10:56 -0400

    added initial js

---
+++ main.js
@@ -0,0 +1,12 @@
+const url = "http://localhost:3000"
+
+const form = document.querySelector(".form")
+
+const movieText = document.querySelector("#New-Movie")
+
+function createMovie (text) {
+    fetch (url) {
+        method: "POST",
+        headers: {"Content-Type":application/JSON}
+    }
+}
